774d3b21118a905def03c552b0a589d7d27239d2
lkbrntgh <lkbrntgh@gmail.com>
2023-07-26 07:13:15 -0400

    Fixing style of user edit modal --> commented

--- app/user/profile/page.jsx
+++ app/user/profile/page.jsx
@@ -3,25 +3,61 @@
 import { useDispatch, useSelector } from "react-redux";
 // import { userinfo } from "@/app/redux/slices/userinfo";
 import { userInfo } from "@/app/redux/slices/userinfo";
+import userupdate from "@/app/redux/slices/userupdate";
 import { unwrapResult } from "@reduxjs/toolkit";
 import { redirect, useRouter } from "next/navigation";
 import Loggedin from "@/app/services/loggedin";
 import React, { useState, useEffect } from "react";
 import { errorToast } from "@/app/services/toast";
 import "@/app/globals.css";
+import { Modal, Button, Box, Typography } from "@mui/material";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function Profile() {
-  //   console.log("id :", id);
   const dispatch = useDispatch();
   const { data, loading, error } = useSelector((state) => state.userInfo);
+  console.log("data :", data);
+  const [user, setUser] = useState({
+    first_name: data && data.first_name,
+    last_name: data && data.last_name,
+    email: data && data.email,
+    phone_number: data && data.phone_number,
+  });
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   useEffect(() => {
     const username = JSON.parse(localStorage.getItem("user")).username;
     dispatch(userInfo(username));
   }, [dispatch]);
 
-  if (data) {
-    console.log("data :", data.groups[0].name);
-  }
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await dispatch(userupdate(user));
+      unwrapResult(res);
+      push("/");
+    } catch (err) {
+      console.log(err);
+      errorToast("خطای سامانه");
+    }
+  };
+
+  // if (data) {
+  //   console.log("data :", data.groups[0].name);
+  // }
 
   if (loading === "loading") {
     return <p>Loading...</p>;
@@ -31,12 +67,24 @@ export default function Profile() {
     return <p>Error: {error}</p>;
   }
 
+  const fields = [
+    { label: 'نام', name: 'first_name' },
+    { label: 'نام خانوادگی', name: 'last_name' },
+    { label: 'آدرس ایمیل', name: 'email' },
+    { label: 'شماره تلفن', name: 'phone_number' },
+  ];
+
+  const ModalContent = ({ user, setUser, onSubmit }) => {
+    const handleChange = (e, fieldName) => {
+      setUser((prevUser) => ({
+        ...prevUser,
+        [fieldName]: e.target.value,
+      }));
+    };
+  };
+
   return (
     <div>
-      <h2 className="text-center text-l digikala">
-        در قسمت زیر میتوانید اطلاعات ناقص پروفایل خود را تکمیل نمایید
-      </h2>
-      <hr className="w-6/12 mx-auto" />
       <div className="flex items-center h-screen w-full justify-center">
         <div className="max-w-xs">
           <div className="bg-white shadow-xl rounded-lg py-3">
@@ -54,38 +102,12 @@ export default function Profile() {
               <table className="text-xs my-3">
                 <tbody>
                   <tr>
-                    {/* {userInfo.phone_number ? (
-                      <td className="px-2 py-2">{userInfo.phone_number}</td>
-                    ) : (
-                      <td>
-                        <input
-                          name="phone_number"
-                          type="text"
-                          value={phoneNumber}
-                          onChange={handleEmailChange}
-                          className="outline-0 rounded p-1 w-full border-2 border-gray-400"
-                        />
-                      </td>
-                    )} */}
                     <td className="px-2 py-2">{data && data.phone_number}</td>
                     <td className="px-2 py-2 text-gray-500 font-semibold digikala">
                       شماره تلفن
                     </td>
                   </tr>
                   <tr>
-                    {/* {userInfo.email ? (
-                      <td className="px-2 py-2">{userInfo.email}</td>
-                    ) : (
-                      <td>
-                        <input
-                          name="email"
-                          type="email"
-                          value={email}
-                          onChange={handlePhoneNumberChange}
-                          className="outline-0 rounded p-1 w-full border-2 border-gray-400"
-                        />
-                      </td>
-                    )} */}
                     <td className="px-2 py-2">{data && data.email}</td>
                     <td className="px-2 py-2 text-gray-500 font-semibold digikala">
                       آدرس ایمیل
@@ -103,6 +125,142 @@ export default function Profile() {
                   </tr>
                 </tbody>
               </table>
+              <Button onClick={handleOpen}>
+                <p className="digikala">ویرایش اطلاعات</p>
+              </Button>
+              
+              
+
+              <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <Box sx={style}>
+                <form onSubmit={onSubmit} className="border-0 sm:border-1 p-10 rounded border-gray-400">
+                  {fields.map((field) => (
+                    <div className="w-full" key={field.name}>
+                      <label
+                        htmlFor={field.name}
+                        className="text-sm font-medium leading-6 text-gray-900 float-right digikala"
+                      >
+                        {field.label}
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          type="text"
+                          id={field.name}
+                          onChange={(e) => handleChange(e, field.name)}
+                          value={user[field.name]}
+                          className="outline-0 rounded p-1 w-full border-2 border-gray-400"
+                        />
+                      </div>
+                    </div>
+                  ))}
+
+                  <div className="mt-5">
+                    <button className="w-full bg-gray-950 text-white w-100 rounded py-2 digikala">
+                      بروزرسانی
+                    </button>
+                  </div>
+                </form>
+                  {/* <form
+                    onSubmit={onSubmit}
+                    className=" border-0  sm:border-1 p-10 rounded border-gray-400"
+                  >
+                    <div className="w-full">
+                      <label
+                        htmlFor="firstname"
+                        className="text-sm font-medium leading-6 text-gray-900 float-right digikala"
+                      >
+                        نام
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          type="firstname"
+                          onChange={(e) =>
+                            setUser((v) => {
+                              return { ...user, first_name: e.target.value };
+                            })
+                          }
+                          value={user.first_name}
+                          className="outline-0 rounded p-1 w-full border-2 border-gray-400"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="w-full">
+                      <label
+                        htmlFor="lastname"
+                        className="text-sm font-medium leading-6 text-gray-900 float-right digikala"
+                      >
+                        نام خانوداگی
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          type="lastname"
+                          onChange={(e) =>
+                            setUser((v) => {
+                              return { ...user, last_name: e.target.value };
+                            })
+                          }
+                          value={user.last_name}
+                          className="outline-0 rounded p-1 w-full border-2 border-gray-400"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="w-full">
+                      <label
+                        htmlFor="email"
+                        className="text-sm font-medium leading-6 text-gray-900 float-right digikala"
+                      >
+                        آدرس ایمیل
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          type="email"
+                          onChange={(e) =>
+                            setUser((v) => {
+                              return { ...user, email: e.target.value };
+                            })
+                          }
+                          value={user.email}
+                          className="outline-0 rounded p-1 w-full border-2 border-gray-400"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="w-full">
+                      <label
+                        htmlFor="phonenumber"
+                        className="text-sm font-medium leading-6 text-gray-900 float-right digikala"
+                      >
+                        شماره تلفن
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          type="phonenumber"
+                          onChange={(e) =>
+                            setUser((v) => {
+                              return { ...user, phone_number: e.target.value };
+                            })
+                          }
+                          value={user.phone_number}
+                          className="outline-0 rounded p-1 w-full border-2 border-gray-400"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="mt-5">
+                      <button className="w-full bg-gray-950 text-white w-100 rounded py-2 digikala">
+                        بروزرسانی
+                      </button>
+                    </div>
+                  </form> */}
+                </Box>
+              </Modal>
             </div>
           </div>
         </div>
